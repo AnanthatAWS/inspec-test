486f2eea003270f00e2361aea47bb5f7a1556bbb
SandadiAWS <34898367+SandadiAWS@users.noreply.github.com>
2018-02-01 17:56:17 -0800

    Update and rename windows.rb to windows_rdp_port_check.rb

--- controls/windows.rb
+++
@@ -1,8 +0,0 @@
-require 'rbconfig'
-is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
-if is_windows
-  describe service('DHCP Client') do
-    it { should be_installed }
-    it { should be_running }
-  end
-end
