ba84592d3d362d342e31ab4134fcc458783b6a77
SandadiAWS <34898367+SandadiAWS@users.noreply.github.com>
2018-02-01 17:48:46 -0800

    Update linux.rb

--- controls/linux.rb
+++ controls/linux.rb
@@ -1,7 +1,8 @@
 require 'rbconfig'
 is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
 if ! is_windows
-  describe file('/var/log/amazon/ssm/amazon-ssm-agent.log') do
-     it { should exist }
+  describe port(22) do
+    it { should be_listening }
+    its('addresses') {should not include '0.0.0.0'}
   end
 end
