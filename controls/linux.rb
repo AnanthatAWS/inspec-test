1540360e22a377163223e5449592544549ebeee5
SandadiAWS <34898367+SandadiAWS@users.noreply.github.com>
2018-02-01 17:52:07 -0800

    Update linux.rb

--- controls/linux.rb
+++ controls/linux.rb
@@ -3,6 +3,6 @@ is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
 if ! is_windows
   describe port(22) do
     it { should be_listening }
-    its('addresses') {should not include '0.0.0.0'}
+    its('addresses') {should_not include '0.0.0.0'}
   end
 end
