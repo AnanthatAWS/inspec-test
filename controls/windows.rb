67a8504710daabe6a8a432f1c91714f389559487
SandadiAWS <34898367+SandadiAWS@users.noreply.github.com>
2018-02-01 17:27:56 -0800

    Update windows.rb

--- controls/windows.rb
+++ controls/windows.rb
@@ -1,4 +1,8 @@
-describe service('DHCP Client') do
-  it { should be_installed }
-  it { should be_running }
+require 'rbconfig'
+is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
+if is_windows
+  describe service('DHCP Client') do
+    it { should be_installed }
+    it { should be_running }
+  end
 end
