fc445e8b7d8b15822182722d7a37ac67ed89cc27
SandadiAWS <34898367+SandadiAWS@users.noreply.github.com>
2018-02-01 17:44:07 -0800

    Update linux.rb

--- controls/linux.rb
+++ controls/linux.rb
@@ -1,9 +1,7 @@
 require 'rbconfig'
 is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
 if ! is_windows
-  if File.exist?('/var/log/amazon/ssm/amazon_ssm.log')
-    puts 'aws log available'
-  else
-    puts 'aws log not available'
+  describe file('/var/log/amazon/ssm/amazon-ssm-agent.log') do
+     it { should exist }
   end
 end
